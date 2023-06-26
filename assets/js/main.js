6a9fb59e317abd49872c8ad767fcddcd880bdbf2
lorgik <sib.lorgik@gmail.com>
2023-06-26 21:59:15 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,32 @@ const slider = document.getElementById("slider");
 const arrowLeft = document.getElementById("arrow-left");
 const arrowRight = document.getElementById("arrow-right");
 
-// console.log(slider.offsetWidth);
+const slides = [].slice.call(slider.children);
+
+const sliderWidth = slider.offsetWidth;
+const sliderGap = 24;
+const sliderSwaps = slides.length - 1;
+
+let counts = sliderSwaps;
+
+const scrollDistance =
+  (sliderWidth - sliderGap * sliderSwaps) / slides.length + sliderGap;
+
+function getMarginLeft() {
+  const marginLeft = slides[0].style.marginLeft;
+  return Number(marginLeft.slice(0, marginLeft.length - 2));
+}
 
 arrowLeft.addEventListener("click", function () {
-  const slides = [].slice.call(slider.children);
-  console.log(slides[0]);
-  slides[0].style.marginLeft = slides[0].style.marginLeft + "440px";
+  if (counts > 1) {
+    slides[0].style.marginLeft = `${getMarginLeft() + scrollDistance}px`;
+    counts--;
+  }
 });
 
 arrowRight.addEventListener("click", function () {
-  const slides = [].slice.call(slider.children);
-  console.log(slides[0]);
-  slides[0].style.marginLeft = "-440px";
+  if (counts < sliderSwaps + 1) {
+    slides[0].style.marginLeft = `${getMarginLeft() - scrollDistance}px`;
+    counts++;
+  }
 });
